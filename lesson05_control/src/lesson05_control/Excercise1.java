9a332f2aa144dd1c0c59c9c7d712db99d8569a9d
Sangwon1233 <tkddnjs3351@naver.com>
2024-09-02 18:40:12 +0900

    소스커밋

--- lesson05_control/src/lesson05_control/Excercise1.java
+++ lesson05_control/src/lesson05_control/Excercise1.java
@@ -2,26 +2,69 @@ package lesson05_control;
 
 public class Excercise1 {
 	public static void main(String[] args) {
+		int sum=0;
 		
-		int sum = 0;
+		for(int i = 1 ; i <=100; i++) {	
 		
-		for(int i=0;i<10;i++) {
-		System.out.printf("5의 배수의 합계는 %d\n",5*i);
-		sum += i;
-			
-		}
-		System.out.printf("%5d",sum);
-	
-		sum = 0;
-		int i =1;
-		
-		while (i<=100) {
-			sum +=i;
-			System.out.printf("i : %d, sum : %d\n" ,i,sum);
-			i++;
-			
-		}
-		System.out.println("합계 : "+sum);
+			if(i%5==0) {
+				sum+=i;
 				
+				
+				
+			}
+		
+		
+		
+		}
+		System.out.println("5의 배수의 합계는 :"+sum);
+		
+		
+		
+		int evenSum=0;
+		int oddSum=0;
+		
+		
+		for(int i=1;i<=100;i++) {
+			
+			if(i%2==0){
+				evenSum+=i;
+				
+			}
+			else {
+				oddSum+=i;
+				
+			}
+		}
+		
+		System.out.println("짝수의 합계는 :"+evenSum);
+		System.out.println("홀수의 합계는 :"+oddSum);
+		
+		for(int x=1; x<=6;x++){
+			
+			for(int y=1;y<=6;y++){
+				
+				if(x+y==6) {
+					
+					System.out.println("("+x+","+y+")");
+				}
+				
+			}
+		}
+		
+		for(int i=1;i<=5;i++) {
+			
+			for(int j=1;j<=i;j++) 
+			
+					
+					System.out.printf("%s",'*');
+					
+					System.out.println();
+			
+		}
+		
 	}
 }
+
+
+
+
