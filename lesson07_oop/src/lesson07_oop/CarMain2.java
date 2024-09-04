15be8acb406c6146714059084ac09f9def68bdbd
Sangwon1233 <tkddnjs3351@naver.com>
2024-09-04 18:43:01 +0900

    소스커밋

--- lesson07_oop/src/lesson07_oop/CarMain2.java
+++ lesson07_oop/src/lesson07_oop/CarMain2.java
@@ -4,7 +4,7 @@ public class CarMain2 {
 	public static void main(String[] args) {
 		//Car 타입의 배열객체 생성
 		
-		Car[] cars =new Car[3];
+		Car[] cars =new Car[3];//null로 만들고 주소를 만들고 cars에 주소 알려주는것을 이 줄에서 함
 		
 		//car 객체 생성
 		Car tico = new Car();
